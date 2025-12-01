178f22e6cfb07d47e55be310c2e288198383010d
Sofia Ruiz Suarez <ruizsuar@fish.utstat.utoronto.ca>
2025-12-01 15:09:52 -0500

    add setup and MANIFIEST

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hierclassifier",                
+    version="0.1",                   
+    packages=find_packages(),       
+    description="A library for performing hierchical classification using CNN.",  
+    author="Sofia Ruiz-Suarez",
+    url="https://github.com/sofiar/Plankton-h-classifier",
+    include_package_data=True
+    )
